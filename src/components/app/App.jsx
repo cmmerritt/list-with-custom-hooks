3d82b33407ce675ee0747d68558833adf47d412b
cmmerritt <christiane.merritt@gmail.com>
2021-08-05 11:58:02 -0700

    add header with link to home

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+import Header from './Header';
 import QueenList from '../queens/QueenList';
 import QueenDetail from '../details/QueenDetail';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={QueenList} />
         <Route exact path="/:id" component={QueenDetail} />
