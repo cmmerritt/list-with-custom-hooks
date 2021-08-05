47e948d9b5dc0d2506ae52b40faa516de120e663
cmmerritt <christiane.merritt@gmail.com>
2021-08-04 23:40:29 -0700

    start App.jsx test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('displays a list of queens on the home page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const ul = await screen.findByRole('list');
+
+    expect(ul).not.toBeEmptyDOMElement();
   });
 });
