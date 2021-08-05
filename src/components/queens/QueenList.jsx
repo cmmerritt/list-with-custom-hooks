74ca48aa346558bd81a2d78224bcd05b70ba3cf2
cmmerritt <christiane.merritt@gmail.com>
2021-08-04 23:34:53 -0700

    add links to detail pages

--- src/components/queens/QueenList.jsx
+++ src/components/queens/QueenList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useQueens } from '../../state/queen';
 import Queen from './Queen';
+import { Link } from 'react-router-dom';
 
 const QueenList = () => {
   const queens = useQueens();
 
   const queenElements = queens.map((queen) => (
     <li key={queen.id}>
-      <Queen {...queen} />
+      <Link to={`/${queen.id}`}>
+        <Queen {...queen} />
+      </Link>
     </li>
   ));
 
