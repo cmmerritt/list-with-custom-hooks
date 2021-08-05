b79377b9aada3eb4c1a618f6eb70c1d157c63b58
cmmerritt <christiane.merritt@gmail.com>
2021-08-05 11:48:02 -0700

    change http to https for netlify

--- src/components/queens/Queen.jsx
+++ src/components/queens/Queen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Queen = ({ name, image, quote }) => (
   <figure>
-    <img src={image} alt={name} height="200px"/>
+    <img src={image} alt={name} height="200px" />
     <figcaption>
       <p>{name}</p>
       <p>&quot;{quote}&quot;</p>
--- src/services/dragQueenApi.js
+++ src/services/dragQueenApi.js
@@ -1,12 +1,12 @@
 
 export const fetchQueens = async () => {
-  const res = await fetch('http://www.nokeynoshade.party/api/queens?limit=10');
+  const res = await fetch('https://www.nokeynoshade.party/api/queens?limit=10');
   const json = await res.json();
   return json;
 };
 
 export const fetchQueenById = async (id) => {
-  const res = await fetch(`http://www.nokeynoshade.party/api/queens/${id}`);
+  const res = await fetch(`https://www.nokeynoshade.party/api/queens/${id}`);
   const json = await res.json();
   return json;
 };
